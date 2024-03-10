693fc3ab92c35e192750ed4ae8d9576405f84bed
Kim <ktran2009a@gmail.com>
2024-03-10 05:49:10 -0700

    Completed nav bar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,11 +3,23 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to='/home'> Home</Link>
-            <Link to='/chatbot'> Chatbot</Link>
+        <div class=" sticky bottom-0 navbar bg-base-100">
+            <div class="flex flex-1 space-x-10 justify-center items-center">
+                <Link to='/home'> <img src="home_button.svg" /></Link>
+                <Link to='/chatbot'> <img src="chatbot_button.svg" /></Link>
+                <Link to='/chatbot'> <img src="profile_button.svg" /></Link>
+            </div>
         </div>
     )
 }
+
+// const Navbar = () => {
+//     return (
+//         <div class="bg-white py-10">
+//             <Link to='/home'> Home</Link>
+//             <Link to='/chatbot'> Chatbot</Link>
+//         </div>
+//     )
+// }
 
 export default Navbar
